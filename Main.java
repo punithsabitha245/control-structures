e400e6bf253c3a13aa1e03fa875ebd5167ed96c0
punithsabitha245 <punith.sabitha@gmail.com>
2026-09-13 17:50:55 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Hello user please enter your enter your name:");
+        String name=sc.nextline();
+        System.out.println("Enter your lucky number");
+        int lucky=sc.nextInt():
+        System.out.println("Hello "+name+", your lucky number"+ lucky +"Shows that you are really hardworking.Keep it up!");
+        
+    }
+}
---
+++ loops.java
@@ -0,0 +1,17 @@
+class Main{
+    public static void main(String[] args){
+        // using for loop
+        for(int i=10; i>1; i--){
+                 System.out.println("The value of 1 is:"+1);
+        }
+
+        //using while loop
+        int j=0;
+        while(j<11){
+          System.out.println("The value of 1 is:"+j);
+          j++;
+            
+        }
+        }
+    }
+}
